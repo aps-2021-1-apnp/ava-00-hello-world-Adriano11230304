79ec29708f201df789d95679f8b7dc4743e96b0f
Adriano11230304 <adriano.al.pereira1@gmail.com>
2021-01-17 20:55:55 -0300

    pronto

--- main.ts
+++ main.ts
@@ -2,6 +2,11 @@ function hello(q: string): void {
   console.log('Hello', q)
 }
 
-const quem: string = 'World'
+function bye(q: string): void {
+  console.log('Bye', q)
+}
+
+const quem: string = 'World!!!'
 
 hello(quem)
+bye(quem)
